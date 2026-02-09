3b42706ea676bd1a9fc260ca0d1833e0d6d983e2
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2026-02-09 14:04:33 +0200

    Add swagger settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Transactions API')
+    .setDescription('This is a simple API to manage transactions')
+    .setVersion('1.0')
+    .addTag('transactions')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,11 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { CreateTransactionDto } from '../dto/create-transaction.dto';
+import { UpdateTransactionDto } from '../dto/update-transaction.dto';
+import { Transaction } from '../entities/transaction.entity';
+import { randomUUID } from 'node:crypto';
+import { create } from 'xmlbuilder2';
 
 @Injectable()
 export class TransactionsService {
+  transactions: Transaction[] = [];
+
   create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+    const transaction = { ...createTransactionDto } as Transaction;
+    transaction.reference = randomUUID();
+    transaction.date = new Date();
+    this.transactions.push(transaction);
+    try {
+      const obj = {
+        TransferInfo: {
+          ...transaction,
+        },
+      };
+      const doc = create(obj);
+      const xml = doc.end({ prettyPrint: true });
+      console.log(xml);
+    } catch (error) {
+      console.error('Error converting transaction to XML:', error);
+    }
+
+    return transaction;
   }
 
   findAll() {
