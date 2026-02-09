758d72504ca00c0633b6e8ac1e6ed1fd3865f15f
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2026-02-09 14:17:53 +0200

    Create initial transaciton data

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,11 @@
-export class CreateTransactionDto {}
+export class CreateTransactionDto {
+  amount: number;
+  currency: string;
+  senderAccountNumber: string;
+  recipientBankCode: string;
+  recipientAccountNumber: string;
+  beneficiaryName: string;
+  notes?: string[];
+  paymentType: string;
+  chargeDetails: string;
+}
--- src/transactions/entities/transaction.entity.ts
+++ src/transactions/entities/transaction.entity.ts
@@ -1,1 +1,13 @@
-export class Transaction {}
+export class Transaction {
+  reference: string;
+  date: Date;
+  amount: number;
+  currency: string;
+  senderAccountNumber: string;
+  recipientBankCode: string;
+  recipientAccountNumber: string;
+  beneficiaryName: string;
+  notes?: string[];
+  paymentType: string;
+  chargeDetails: string;
+}
